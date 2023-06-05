1fd6d7b206e142c150358658c9272bbae2eb7e8d
BahamaAtlas <116192502+BahamaAtlas@users.noreply.github.com>
2023-06-04 20:51:18 -0400

    Update create_bus_route_csvs.py

--- src/create_bus_route_csvs.py
+++ src/create_bus_route_csvs.py
@@ -1,43 +1,29 @@
 import csv
 
-def create_bus_route_csvs(pdf_text):
-    # Split the text into individual routes
-    routes = pdf_text.split("Route #")[1:]
+def create_bus_route_csvs(route):
+    #create index of routes
+    route_number_match = ['1', '1A', '2', '2A', '3', '3A', '4', '4A', '5', '5A', '6', '6A', '7', '7A', '8', '8A', '9', '9A', '9B', '10', '10A', '10B', '11', '11A', '12', '14', '14A', '15', '15A', '16', '16A', '16B', '17', '17A', '18', '19', '20', '21', '21A', '22', '22A', '23']   
+    # Extract the service area
+    service_area = route.split('\n')[0].strip()
+    # Extract the route number and starting point
+    route_number = route[0]
+    starting_point = route.split('\n')[0]
+    # Extract the street names
+    street_names = route.split('\n')[1].split('/')
+    
+    # Create a new CSV file for the route
+    number = routes.index(route)
+    csv_filename = f'Route_{route_number_match[number]}.csv'
+    
+    df = pd.DataFrame(columns = ['Service Area', 'Route Number', 'Starting Point', 'Street Names'])
+    df['Street Names'] = street_names
+    df['Service Area'] = service_area
+    df['Route Number'] = route_number
+    df['Starting Point'] = starting_point
+    
+    df.to_csv(f'{csv_filename}')
+    print(route)
 
-    # Iterate over each route
-    for route in routes:
-        # Extract the service area
-        service_area = route.split('\n')[0].strip()
-
-        # Extract the route number and starting point
-        route_info = route.split('\n')[1].strip().split(' ')
-        route_number = route_info[0]
-        starting_point = ' '.join(route_info[2:])
-
-        # Extract the street names
-        street_names = route.split('\n')[2].split('/')
-        street_names = [name.strip() for name in street_names]
-
-        # Create a new CSV file for the route
-        csv_filename = f'Route_{route_number}.csv'
-        with open(csv_filename, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(['Service Area', 'Route Number', 'Starting Point', 'Street Names'])
-            writer.writerow([service_area, route_number, starting_point, '/'.join(street_names)])
-
-        print(f'Created CSV file for Route {route_number}')
-
-# Example usage
-pdf_text = """
-Official Bus Routes
-
-Englerston
-Route #1 Starting Point: East Hill Street
-Street 1/Street 2/Street 3
-
-Downtown
-Route #2 Starting Point: Main Street
-Street 4/Street 5/Street 6
-"""
-
-create_bus_route_csvs(pdf_text)
+for i in range(0,42):
+    route = routes[i]
+    router_csv(route)
