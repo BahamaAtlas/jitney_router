e78015efb8e200ab46c1ea3ffcb34e2786b3132f
BahamaAtlas <116192502+BahamaAtlas@users.noreply.github.com>
2023-06-08 15:21:06 -0400

    Update create_route_shapefile.py

--- src/create_route_shapefile.py
+++ src/create_route_shapefile.py
@@ -1,31 +1,30 @@
 import pandas as pd
 import geopandas as gpd
 import osmnx as ox
-from shapely.geometry import LineString
+from shapely.geometry import Point
 
-def generate_route_shapefile:
-  # Read the CSV file into a pandas DataFrame
-  df = pd.read_csv('streets.csv')
+# Read the CSV file into a pandas DataFrame
+df = pd.read_csv('streets.csv')
 
-  # Create an empty list to store LineString geometries
-  geometry = []
+# Create an empty list to store Point geometries
+geometry = []
 
-  # Iterate over the street names in the CSV
-  for street_name in df['street_names']:
-      # Retrieve the street network for the given street name
-      graph = ox.graph_from_place(street_name, network_type='all_private')
+# Retrieve the street network for Nassau, Bahamas
+graph = ox.graph_from_place("Nassau, Bahamas", network_type='all_private', which_result=2)
+
+# Iterate over the street names in the CSV
+for street_name in df['street_names']:
+    # Find the nearest network nodes to the given street name
+    nodes = ox.distance.nearest_nodes(graph, df['latitude'], df['longitude'], method='euclidean')
     
-     # Extract the coordinates for the street network
-      nodes, edges = ox.graph_to_gdfs(graph)
+    # Create Point geometries from the nearest nodes
+    points = [Point(graph.nodes[node]['x'], graph.nodes[node]['y']) for node in nodes]
     
-      # Create a LineString from the extracted coordinates
-      line = LineString(nodes.geometry.values)
-    
-      # Add the LineString to the geometry list
-      geometry.append(line)
+    # Add the Point geometries to the geometry list
+    geometry.extend(points)
 
-  # Create a GeoDataFrame from the DataFrame and LineString geometries
-  gdf = gpd.GeoDataFrame(df, geometry=geometry, crs='EPSG:4326')
+# Create a GeoDataFrame from the DataFrame and Point geometries
+gdf = gpd.GeoDataFrame(df, geometry=geometry, crs='EPSG:4326')
 
-  # Save the GeoDataFrame as a shapefile
-  gdf.to_file('line_layer.shp', driver='ESRI Shapefile')
+# Save the GeoDataFrame as a shapefile
+gdf.to_file('point_layer.shp', driver='ESRI Shapefile')
